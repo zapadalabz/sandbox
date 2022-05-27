dd75c9f9d86ca23049dc3bd268b47f448725780b
zedmed1 <56368033+zedmed1@users.noreply.github.com>
2022-05-27 11:06:21 -0400

    update

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const port = process.env.PORT || 5000; //Line 3
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
 // create a GET route
 app.get('/', (req, res) => { //Line 9
-    res.send("HELLO THERE")
-    // res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    // res.send("HELLO THERE")
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
   }); //Line 11
 
 // create a GET route
