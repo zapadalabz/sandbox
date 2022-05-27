d1da781a8838d877a80d85fa6dae2403d584e795
zedmed1 <56368033+zedmed1@users.noreply.github.com>
2022-05-27 14:16:35 -0400

    update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000; //Line 3
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
-app.use(require("./routes/record"));
+app.use(require(express.static(path.join(__dirname, 'routes/record'))));
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
