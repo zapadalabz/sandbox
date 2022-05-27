cc604758964e87b18d89dd8e584cac63fa80c55a
zedmed1 <56368033+zedmed1@users.noreply.github.com>
2022-05-27 15:32:41 -0400

    update

--- server.js
+++ server.js
@@ -6,23 +6,26 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
-
+const dbo = require("./db/conn");
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-// create a GET route
-app.get('/', (req, res) => { //Line 9
-    // res.send("HELLO THERE")
-
-    // res.send(__dirname)
-    res.sendFile(path.join(__dirname+"/client/build/index.html"));
-  }); //Line 11
 
 // create a GET route
 app.get('/express_backend', (req, res) => { //Line 9
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
 }); //Line 11
 
+app.get('*', (req, res) => { //Line 9
+  res.sendFile(path.join(__dirname+"/client/build/index.html"));
+}); //Line 11
 
 
 // This displays message that the server running and listening to specified port
-app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
+app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+ 
+  });
+  console.log(`Server is running on port: ${port}`);
+});
