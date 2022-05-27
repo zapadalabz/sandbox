d1a7137b06e79a343c0d38ca73ce109dfca3a9ec
zedmed1 <56368033+zedmed1@users.noreply.github.com>
2022-05-27 11:31:34 -0400

    update

--- server.js
+++ server.js
@@ -2,22 +2,23 @@ const express = require('express'); //Line 1
 const app = express(); //Line 2
 const path = require('path');
 const port = process.env.PORT || 5000; //Line 3
-app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // create a GET route
-app.get('/', (req, res) => { //Line 9
-    // res.send("HELLO THERE")
 
-    
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  }); //Line 11
 
 // create a GET route
 app.get('/express_backend', (req, res) => { //Line 9
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
 }); //Line 11
 
+app.get('*', (req, res) => { //Line 9
+    // res.send("HELLO THERE")
 
+
+    res.sendFile(path.join(__dirname+"/client/build/index.html"));
+  }); //Line 11
 
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
