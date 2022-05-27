153e7b61e936717325de0c8af2bfb177510e4418
zedmed1 <56368033+zedmed1@users.noreply.github.com>
2022-05-27 11:21:57 -0400

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express'); //Line 1
 const app = express(); //Line 2
 const path = require('path');
 const port = process.env.PORT || 5000; //Line 3
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 // create a GET route
 app.get('/', (req, res) => { //Line 9
